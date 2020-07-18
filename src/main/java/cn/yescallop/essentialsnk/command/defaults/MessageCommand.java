45ace6e27dfd0088fda0786fae9ac9ea5eb950d6
PetteriM1 <petteri.malkavaara1@gmail.com>
2020-07-19 00:06:01 +0300

    Message command improvements

--- src/main/java/cn/yescallop/essentialsnk/command/defaults/MessageCommand.java
+++ src/main/java/cn/yescallop/essentialsnk/command/defaults/MessageCommand.java
@@ -4,10 +4,13 @@ import cn.nukkit.Player;
 import cn.nukkit.command.CommandSender;
 import cn.nukkit.command.data.CommandParamType;
 import cn.nukkit.command.data.CommandParameter;
+import cn.nukkit.lang.TranslationContainer;
 import cn.nukkit.utils.TextFormat;
 import cn.yescallop.essentialsnk.EssentialsAPI;
 import cn.yescallop.essentialsnk.Language;
 import cn.yescallop.essentialsnk.command.CommandBase;
+
+import java.util.Objects;
 
 public class MessageCommand extends CommandBase {
 
@@ -33,18 +36,41 @@ public class MessageCommand extends CommandBase {
         }
 
         Player player = api.getServer().getPlayer(args[0]);
+
         if (player == null) {
             sender.sendMessage(TextFormat.RED + Language.translate("commands.generic.player.notfound", args[0]));
-            return false;
+            return true;
+        }
+
+        if (Objects.equals(player, sender)) {
+            sender.sendMessage(new TranslationContainer(TextFormat.RED + "%commands.message.sameTarget"));
+            return true;
+        }
+
+        if (sender instanceof Player) {
+            if (api.isIgnoring(((Player) sender).getUniqueId(), player.getUniqueId())) {
+                sender.sendMessage(TextFormat.RED + "This player doesn't allow private messages from you");
+                return true;
+            }
         }
 
         StringBuilder builder = new StringBuilder();
         for (int i = 1; i < args.length; i++) {
-            builder.append(args[i]).append(" ");
+            builder.append(args[i]).append(' ');
         }
 
-        sender.sendMessage(Language.translate("commands.message.toformat", player.getName(), builder.toString()));
-        player.sendMessage(Language.translate("commands.message.fromformat", sender.getName(), builder.toString()));
+        if (builder.length() > 255) {
+            sender.sendMessage(TextFormat.RED + "The message is too long");
+            return true;
+        } else if (builder.length() > 0) {
+            builder = new StringBuilder(builder.substring(0, builder.length() - 1));
+        }
+
+        String m = "§5" + TextFormat.clean(builder.toString());
+        String displayName = (sender instanceof Player ? ((Player) sender).getDisplayName() : sender.getName());
+
+        sender.sendMessage('[' + sender.getName() + " §7-> §f" + player.getDisplayName() + "] " + m);
+        player.sendMessage('[' + displayName + " §7-> §f" + player.getName() + "] " + m);
 
         api.getLastMessagedPlayers().put(sender.getName(), player.getName());
         api.getLastMessagedPlayers().put(player.getName(), sender.getName());
