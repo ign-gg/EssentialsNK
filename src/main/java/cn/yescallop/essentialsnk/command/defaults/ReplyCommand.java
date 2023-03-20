b7cc7410cb3069263e621d3b020691aaf7ac453f
Petteri <26197131+PetteriM1@users.noreply.github.com>
2023-03-20 23:46:27 +0200

    Update ReplyCommand.java

--- src/main/java/cn/yescallop/essentialsnk/command/defaults/ReplyCommand.java
+++ src/main/java/cn/yescallop/essentialsnk/command/defaults/ReplyCommand.java
@@ -1,5 +1,7 @@
 package cn.yescallop.essentialsnk.command.defaults;
 
+import cn.nukkit.Player;
+import cn.nukkit.Server;
 import cn.nukkit.command.CommandSender;
 import cn.nukkit.command.data.CommandParamType;
 import cn.nukkit.command.data.CommandParameter;
@@ -36,28 +38,45 @@ public class ReplyCommand extends CommandBase {
         }
 
         String playerName = api.getLastMessagedPlayers().get(sender.getName());
-        CommandSender target = api.getServer().getPlayer(playerName);
+        Player player = api.getServer().getPlayerExact(playerName);
 
-        if (target == null) {
-            if (playerName.equalsIgnoreCase("CONSOLE")) {
-                target = api.getServer().getConsoleSender();
-            } else {
-                sender.sendMessage(TextFormat.RED + Language.translate("commands.generic.player.notfound", playerName));
-                api.getLastMessagedPlayers().remove(sender.getName());
-                return false;
+        if (player == null) {
+            sender.sendMessage(TextFormat.RED + Language.translate("commands.generic.player.notfound", playerName));
+            api.getLastMessagedPlayers().remove(sender.getName());
+            return false;
+        }
+
+        if (sender instanceof Player) {
+            Player sePlayer = (Player) sender;
+            if (api.isMuted(sePlayer)) {
+                sePlayer.sendMessage(Language.translate("commands.generic.muted", api.getUnmuteTimeMessage(sePlayer)));
+            }
+            if (api.isIgnoring(player.getUniqueId(), sePlayer.getUniqueId())) {
+                sender.sendMessage(TextFormat.RED + "This player doesn't allow private messages from you");
+                return true;
             }
         }
 
         StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < args.length; i++) {
-            builder.append(args[i]).append(" ");
+        for (String arg : args) {
+            builder.append(arg).append(' ');
         }
 
-        sender.sendMessage(Language.translate("commands.message.toformat", target.getName(), builder.toString()));
-        target.sendMessage(Language.translate("commands.message.fromformat", sender.getName(), builder.toString()));
+        if (builder.length() > (Server.sbpeTweaks ? 255 : 200)) {
+            sender.sendMessage(TextFormat.RED + "The message is too long");
+            return true;
+        } else if (builder.length() > 0) {
+            builder = new StringBuilder(builder.substring(0, builder.length() - 1));
+        }
 
-        api.getLastMessagedPlayers().put(sender.getName(), target.getName());
-        api.getLastMessagedPlayers().put(target.getName(), sender.getName());
+        String text = TextFormat.clean(builder.toString()).replaceAll("[\uE000-\uE0EA\n]", "?");
+        String displayName = (sender instanceof Player ? ((Player) sender).getDisplayName() : sender.getName());
+
+        sender.sendMessage("§f[§7" + sender.getName() + " -> " + player.getDisplayName() + "§f] §r" + text);
+        player.sendMessage("§f[§7" + displayName + " -> " + player.getName() + "§f] §r" + text);
+
+        api.getLastMessagedPlayers().put(sender.getName(), player.getName());
+        api.getLastMessagedPlayers().put(player.getName(), sender.getName());
         return true;
     }
 }
